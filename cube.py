2d8fc42624f3b818eb7c764889939c383a4b3db0
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-10 22:11:54 +0600

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,52 @@
+def energy(epm, x):
+    while x!=0:
+        energy+=epm
+def cube(condition):
+    if condition == 0:
+        energy(0)
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
