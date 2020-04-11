c8c4c34e9874a6cd391a32201e58cfd7747d6fa4
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 18:57:30 +0600

    Update script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -3,9 +3,13 @@
 import config 
 import telebot 
 tb = telebot.TeleBot(config.token)
-@tb.message_handler(commands=['sigarette'])
+@tb.message_handler(commands=['start', 'exit'])
 def answer(message):
-    tb.send_message(message.chat.id, "хочу сигуууууу")
+    tb.send_message(message.chat.id, "Hello, i'm test bot, which was made by Alexandra Netskaya")
+    tb.send_message(message.chat.id, "I can repeat your messages, if you want it, you can write '/echo' for me"
+@tb.message_handler(commands=['echo'])
+def answer(message);
+    
 
 tb.polling(none_stop=False, interval = 0, timeout = 20)
 
