d7d06b3037e489bf8a50452f97877faa156b196e
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 12:57:32 +0600

    Update script.py

--- script.py
+++ script.py
@@ -5,5 +5,7 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"]) 
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе 
     bot.send_message(message.chat.id, message.text) 
-    if __name__ == '__main__': 
-        bot.infinity_polling()
+   
+
+if __name__ == '__main__': 
+    bot.infinity_polling()
