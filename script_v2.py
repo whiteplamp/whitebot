706b3bda98b9f88ed1307d822ba16a54055608f0
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 19:17:58 +0600

    Update script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -6,7 +6,7 @@ tb = telebot.TeleBot(config.token)
 @tb.message_handler(commands=['start'])
 def answer(message):
     tb.send_message(message.chat.id, "Hello, i'm test bot, which was made by Alexandra Netskaya")
-    tb.send_message(message.chat.id, "I can repeat your messages, if you want it, you can write '/echo' for me"
+    tb.send_message(message.chat.id, "I can repeat your messages, if you want it, you can write '/echo' for me")
 @tb.message_handler(commands=['echo'])
 def loop(message):
     tb.send_message(message.chat.id, 'Your next message will be repeated by me')
