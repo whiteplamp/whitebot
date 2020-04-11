33a7e2b471cb8dc57b0e21fe8dbf2d522eaffb49
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 19:14:55 +0600

    Update script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -8,7 +8,7 @@ def answer(message):
     tb.send_message(message.chat.id, "Hello, i'm test bot, which was made by Alexandra Netskaya")
     tb.send_message(message.chat.id, "I can repeat your messages, if you want it, you can write '/echo' for me"
 @tb.message_handler(commands=['echo'])
-def answer(message):
+def loop(message):
     tb.send_message(message.chat.id, 'Your next message will be repeated by me')
     @tb.message_handler(commands!=['exit'])
     answer(message)
