470cf0e38891b9eace264f45266aa4f81c88fd34
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 16:11:13 +0600

    Update script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -5,5 +5,5 @@ import telebot
 bot = telebot.TeleBot(config.token) 
 @bot.message_handler(commands=["test"]) 
 def send_hello(message): # Название функции не играет никакой роли, в принципе 
-    bot.send_message(message, "Whatssup?") 
+    bot.send_message(message.chat_id, "Хочу сижжжжжку?") 
 bot.polling()
