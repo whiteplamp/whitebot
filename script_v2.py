87a7d95ead93f5e27081b8cbdcb1f2ff29f4b84a
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 19:14:10 +0600

    Update script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -3,7 +3,7 @@
 import config 
 import telebot 
 tb = telebot.TeleBot(config.token)
-@tb.message_handler(commands=['start', 'exit'])
+@tb.message_handler(commands=['start'])
 def answer(message):
     tb.send_message(message.chat.id, "Hello, i'm test bot, which was made by Alexandra Netskaya")
     tb.send_message(message.chat.id, "I can repeat your messages, if you want it, you can write '/echo' for me"
