3ee4d794a07bf17b450d58e28867d0c20dffb125
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-11 22:23:47 +0600

    Update bot_sc.py

--- bot_sc.py
+++ bot_sc.py
@@ -5,15 +5,14 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    chat_id = message.chat.id
     bot.send_message(message.chat.id, "Hello, i'm bot, my developer is Alexandra Netskaya, I can do only one thing. And it is write message.")
     bot.send_message(message.chat.id, 'Choose action')
-markup = types.ReplyKeyboardMarkup()
-a = types.KeyboardButton('echo')
-b = types.KeyboardButton('hey')
-markup.row(a)
-markup.row(b)
-bot.send_message(chat_id, "Choose letter:", reply_markup = markup)
+    markup = types.ReplyKeyboardMarkup()
+    a = types.KeyboardButton('echo')
+    b = types.KeyboardButton('hey')
+    markup.row(a)
+    markup.row(b)
+    bot.send_message(chat_id, "Choose letter:", reply_markup = markup)
 
 
 bot.polling(none_stop=False, interval = 0, timeout = 20)
