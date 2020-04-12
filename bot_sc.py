a7184aea277caf0e04001e76dec09c20831be4ea
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-12 14:49:07 +0545

    Update bot_sc.py

--- bot_sc.py
+++ bot_sc.py
@@ -8,11 +8,16 @@ def start(message):
     bot.send_message(message.chat.id, "Hello, i'm bot, my developer is Alexandra Netskaya, I can do only one thing. And it is write message.")
     bot.send_message(message.chat.id, 'Choose action')
     markup = types.ReplyKeyboardMarkup()
-    a = types.KeyboardButton('echo')
-    b = types.KeyboardButton('hey')
-    markup.row(a)
-    markup.row(b)
+    a = types.KeyboardButton('1')
+    b = types.KeyboardButton('2')
+    c = types.KeyboardButton('3')
+    d = types.KeyboardButton('4')
+    markup.row(a,b)
+    markup.row(c,d)
     bot.send_message(message.chat.id, "Choose letter:", reply_markup = markup)
+    mes = message.text
+    if mes == '1':
+        bot.send_message(message.chat.id, '1')
 
 
 bot.polling(none_stop=False, interval = 0, timeout = 20)
