dc62929db618b2cff927d8ccd46e002d63590eec
whiteplamp <63465853+whiteplamp@users.noreply.github.com>
2020-04-12 15:06:18 +0545

    Update bot_sc.py

--- bot_sc.py
+++ bot_sc.py
@@ -8,24 +8,27 @@ bot = telebot.TeleBot(config.token)
 def start(message):
     bot.send_message(message.chat.id, "Hello, i'm bot, my developer is Alexandra Netskaya, I can do only one thing. And it is write message.")
     bot.send_message(message.chat.id, 'Choose action')
+    
     markup = types.ReplyKeyboardMarkup()
     a = types.KeyboardButton('1')
     b = types.KeyboardButton('2')
     c = types.KeyboardButton('3')
-    d = types.KeyboardButton('4')
+    d = types.KeyboardButton('/back')
     markup.row(a,b)
-    markup.row(c,d)
+    markup.row(c)
+    markup.row(d)
     bot.send_message(message.chat.id, "Choose letter:", reply_markup = markup)
     @bot.message_handler(content_types=['text'])
     def test_message(message):
-        if message == '1':
+        if message == '/back':
+            break
+        elif message == '1':
             bot.send_message(message.chat.id, '1')
         elif message == '2':
             bot.send_message(message.chat_id, '1')
         bot.send_message(message.chat.id, '/start')
-        break()
-    break()
-
+    if message == '/back':
+            break   
 
 
 bot.polling(none_stop=False, interval = 0, timeout = 20)
